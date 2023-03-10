03076f648f21e2a2b1d5d5b48bd82974ab979195
seif-maachaoui <seifeddine.maachaoui@laplateforme.io>
2023-03-10 17:01:31 +0100

    Modification du fichier Job05 dans le dossier Jour03, je rajoute une variable qui vérifie si un nombre est premier ou non

--- Jour03/Job05.py
+++ Jour03/Job05.py
@@ -1,15 +1,21 @@
 #Je souhaite afficher les nombres premiers jusqu'à 1000
 # D'abord je commence par déclarer une première boucle qui affiche les nombres de 2 jusqu'à 1000
+
+est_premier = True
+
 for i in range (2, 10):
     #Ensuite je créer une autre boucle qui elle va permettre de vérifier si les nombres sont premiers ou non
     for n in range(2, i):
         #Si le reste du nombre divisé par i est égal à zéro alors il ne sera pas premier
         if i%n == 0 :
             print(i, "n'est pas premier")
+            est_premier = False
+            break
         #Sinon il sera alors premier
-        else:
-            print(i, "est premier")
-    
+    if (est_premier == True):
+        print(i, 'est permier')
+    #On change l'état de la variable est_premier en True, pour que l'opération puisse se réitérer.
+    est_premier = True    
 
 
 
